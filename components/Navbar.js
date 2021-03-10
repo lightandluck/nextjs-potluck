1c79aa08186c6c601cfa26f1b4d33e1a609555aa
Kevin Nguyen <kevin.nguyen@lightandluck.com>
2021-03-10 13:15:47 -0800

    Fix class -> className in Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -17,7 +17,7 @@ const NavBar = () => {
           aria-expanded='false'
           aria-label='Toggle navigation'
         >
-          <span class='navbar-toggler-icon'></span>
+          <span className='navbar-toggler-icon'></span>
         </button>
         <div className='collapse navbar-collapse' id='navbarSupportedContent'>
           <ul className='navbar-nav mr-auto'>
