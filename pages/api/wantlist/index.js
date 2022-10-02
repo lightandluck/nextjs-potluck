0ce15b0c51d2253edaedc4a12ab02f396f6665c6
Kevin Nguyen <kevin.nguyen@lightandluck.com>
2022-10-02 12:27:49 -0700

    Renamed testwants.txt to wantlist.txt

--- pages/api/wantlist/index.js
+++ pages/api/wantlist/index.js
@@ -1,12 +1,15 @@
 import fs from 'fs';
 
+// TODO: This is obviously very dangerous reading and writing to server
+// without much error handling. But is a quick hack to edit the wantlist
+// to run TradeMax results.
 export default async function handler(req, res) {
   const { method } = req;
 
   switch (method) {
     case 'GET':
       try {
-        await fs.readFile('./public/scripts/testwants.txt', (err, data) => {
+        await fs.readFile('./public/scripts/wantlist.txt', (err, data) => {
           console.log(data);
           res.status(200).send(data);
         });
@@ -15,17 +18,21 @@ export default async function handler(req, res) {
       }
       break;
     case 'POST':
-      // writeFile('message.txt', 'Hello Node.js', 'utf8', callback);
       try {
         const wantlist = req.body;
 
         if (wantlist && wantlist.length > 0) {
-          fs.writeFile('./public/scripts/test.txt', wantlist, 'utf8', (err) => {
-            if (err) throw err;
-            res.send('File saved!');
-          });
+          fs.writeFile(
+            './public/scripts/wantlist.txt',
+            wantlist,
+            'utf8',
+            (err) => {
+              if (err) throw err;
+              res.send('File saved!');
+            }
+          );
         } else {
-          throw new Error('Request body did not contain name field.');
+          throw new Error('Request body did not contain data.');
         }
       } catch (error) {
         res.status(400).json({ name: error.name, message: error.message });
