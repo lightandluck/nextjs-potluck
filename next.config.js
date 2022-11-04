892ee4bd35d889f3a59c15294358e0b426a671d3
Kevin Nguyen <kevin.nguyen@lightandluck.com>
2022-11-03 23:15:21 -0700

    Set config target to serverless

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   productionBrowserSourceMaps: true,
+  target: 'serverless',
 };
--- pages/api/wantlist/index.js
+++ pages/api/wantlist/index.js
@@ -9,12 +9,9 @@ export default async function handler(req, res) {
   switch (method) {
     case 'GET':
       try {
-        await fs.readFile(
-          '/var/task/.next/server/public/scripts/wantlist.txt',
-          (err, data) => {
-            res.status(200).send(data);
-          }
-        );
+        await fs.readFile('.public/scripts/wantlist.txt', (err, data) => {
+          res.status(200).send(data);
+        });
       } catch (error) {
         res.status(400).json({ name: error.name, message: error.message });
       }
@@ -22,15 +19,19 @@ export default async function handler(req, res) {
     case 'POST':
       try {
         const wantlist = req.body;
-        res.send(__dirname);
-        // if (wantlist && wantlist.length > 0) {
-        //   fs.writeFile('./scripts/wantlist.txt', wantlist, 'utf8', (err) => {
-        //     if (err) throw err;
-        //     res.send('File saved!');
-        //   });
-        // } else {
-        //   throw new Error('Request body did not contain data.');
-        // }
+        if (wantlist && wantlist.length > 0) {
+          fs.writeFile(
+            './public/scripts/wantlist.txt',
+            wantlist,
+            'utf8',
+            (err) => {
+              if (err) throw err;
+              res.send('File saved!');
+            }
+          );
+        } else {
+          throw new Error('Request body did not contain data.');
+        }
       } catch (error) {
         res.status(400).json({ name: error.name, message: error.message });
       }
