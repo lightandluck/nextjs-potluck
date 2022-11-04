f4cfd9787177a83aa163dd492f46ac3871baedd0
Kevin Nguyen <kevin.nguyen@lightandluck.com>
2022-11-04 12:02:54 -0700

    Use relative path for wantlist api call

--- public/scripts/runTrade.js
+++ public/scripts/runTrade.js
@@ -35,7 +35,7 @@ function processmessage(e) {
 }
 
 function doit() {
-  let url = 'https://nextjs-potluck.netlify.app/api/wantlist';
+  let url = './api/wantlist';
   outframe = document.getElementById('output');
   progressframe = document.getElementById('progress');
   runnow = document.getElementById('runnow');
