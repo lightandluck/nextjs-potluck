e21669b6878487d430cedc9728c306cc42391644
Kevin Nguyen <lightandluck@protonmail.com>
2023-09-04 22:03:27 -0300

    Copy third-party libraries to project

--- pages/_app.js
+++ pages/_app.js
@@ -14,18 +14,21 @@ export default function MyApp({ Component, pageProps }) {
           content='width=device-width, initial-scale=1, shrink-to-fit=no'
         />
         <link rel='icon' href='/favicon.ico' />
-        <script
-          src='https://code.jquery.com/jquery-3.3.1.slim.min.js'
+        <script src='/lib/jquery-3.3.1.slim.min.js'></script>
+        {/* original path: https://code.jquery.com/jquery-3.3.1.slim.min.js 
           integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo'
-          crossOrigin='anonymous'></script>
-        <script
-          src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'
+          crossOrigin='anonymous'
+        */}
+        <script src='/lib/popper.min.js'></script>
+        {/* original path: https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js 
           integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1'
-          crossOrigin='anonymous'></script>
-        <script
-          src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'
+          crossOrigin='anonymous'
+        */}
+        <script src='/lib/bootstrap.min.js'></script>
+        {/* original path: https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js 
           integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM'
-          crossOrigin='anonymous'></script>
+          crossOrigin='anonymous'
+        */}
         <script
           src='https://upload-widget.cloudinary.com/global/all.js'
           crossOrigin='anonymous'></script>
